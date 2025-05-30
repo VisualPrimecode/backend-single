743bd1935cf8ec25b91aef6cd05935aae3ffaa51
Md-Imran75 <ih912137@gmail.com>
2025-05-31 02:30:30 +0600

    milesstone 4.0

--- public/widget.js
+++ public/widget.js
@@ -18,8 +18,8 @@
     return;
   }
 
-  const backendURL = 'http://localhost:5000';   // https://nuvro-dtao9.ondigitalocean.app🔁 Replace with your production URL
-  const frontendURL = 'http://localhost:5173';  // https://chatnuvroai.vercel.app 🔁 Replace with your production URL
+  const backendURL = 'https://nuvro-dtao9.ondigitalocean.app';   // https://nuvro-dtao9.ondigitalocean.app🔁 Replace with your production URL
+  const frontendURL = 'https://chatnuvroai.vercel.app';  // https://chatnuvroai.vercel.app 🔁 Replace with your production URL
 
   // ✅ Step 1: Validate API key & domain
   fetch(
